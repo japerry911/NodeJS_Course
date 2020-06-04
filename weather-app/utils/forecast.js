716f1e6f1e2b932f91578a58363556c8bfb6eaed
japerry911 <jack.e.perry@outlook.com>
2020-06-04 11:17:37 -0600

    web-server hello express

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('postman-request');
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=9e22a7893483b7496c974f7e29b2fe0d&query=${latitude},${longitude}&units=f`;
 
-    request(url, { json: true }, (error, response) => {
+    request(url, { json: true }, (error, { body }) => {
         if (error) {
             callback('Error connecting to the weather data server.', undefined);
-        } else if (response.body.error) {
-            callback(response.body.error, undefined);
+        } else if (body.error) {
+            callback(body.error, undefined);
         } else {
-            callback(undefined, `It's currently ${response.body.current.temperature} degrees out. There is a ${response.body.current.precip}% of rain.`);
+            callback(undefined, `It's currently ${body.current.temperature} degrees out. There is a ${body.current.precip}% of rain.`);
         }
     });
 };
