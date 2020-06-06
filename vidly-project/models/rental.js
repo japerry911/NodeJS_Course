886a7243794729ba188b808882f75943cb75f57e
japerry911 <jack.e.perry@outlook.com>
2020-06-06 10:32:15 -0600

    Rental model created

---
+++ vidly-project/models/rental.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const rentalSchema = new mongoose.Schema({
+    customer: {
+        type: new mongoose.Schema({
+            name: {
+                type: String,
+                required: true,
+                minlength: 5,
+                maxlength: 50
+            },
+            isGold: {
+                type: Boolean,
+                required: true
+            },
+            phone: {
+                type: String,
+                required: false,
+                minlength: 12,
+                maxlength: 12
+            }
+        }),
+        required: true
+    },
+    movie: {
+        type: new mongoose.Schema({
+            title: {
+                type: String,
+                required: true,
+                minlength: 2,
+                maxlength: 50
+            },
+            dailyRentalRate: {
+                type: Number,
+                required: true
+            }
+        }),
+        required: true
+    },
+    dateOut: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    dateReturn: {
+        type: Date
+    },
+    rentalFee: {
+        type: Number,
+        min: 0
+    }
+});
+
+const Rental = mongoose.model('Rental', rentalSchema);
+
+function validateRental(rental) {
+    const schema = {
+        customerId: Joi.string().required(),
+        movieId: Joi.string().required()
+    };
+
+    return Joi.validate(rental, schema);
+}
+
+module.exports.Rental = Rental;
+module.exports.validateRental = validateRental;
