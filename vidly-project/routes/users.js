bd6ff6cdc9e974b18e157c1a74bec15d0b34c8fe
japerry911 <jack.e.perry@outlook.com>
2020-06-08 13:25:15 -0600

    /me route added for users, get route

--- vidly-project/routes/users.js
+++ vidly-project/routes/users.js
@@ -1,9 +1,14 @@
-const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { validateUser, User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+
+router.get('/me', auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
+});
 
 router.post('/', async (req, res) => {
     const { error } = validateUser(req.body);
